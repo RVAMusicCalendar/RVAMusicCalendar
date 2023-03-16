ebe54eccb804427d74b4da86204d92b2557e8c7f
jzheadley <zephyrinkg@gmail.com>
2023-03-15 20:15:41 -0400

    Had to create my own clear_calendar method.  Now using the same calendar every time. Working on filtering out non RVA events (👀😠Broadberry)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 
 from alive_progress import alive_it, alive_bar
 from gcsa.attachment import Attachment as GCalAttachment
-from gcsa.calendar import Calendar
 from gcsa.event import Event as GCalEvent
 from gcsa.google_calendar import GoogleCalendar
 from google.oauth2.service_account import Credentials
@@ -14,7 +13,8 @@ from scrapers.broadberryScraper import get_broadberry_event_urls
 from scrapers.etixScraper import etix_event_details_scraper
 from scrapers.theNationalScraper import get_national_event_urls, the_national_details_scraper
 
-from apiclient import discovery
+rva_music_calendar_id = "810rm4o2829cdhurresns7a4cc@group.calendar.google.com"
+
 
 def get_event_details(driver, event_url):
     driver.get(event_url)
@@ -28,11 +28,11 @@ def get_event_details(driver, event_url):
     return scraped_event
 
 
-def addCalendarEvents(calendarService, calendar, events):
+def addCalendarEvents(calendarService, events):
     with alive_bar(total=len(events), dual_line=True, title='Creating Google Events', ctrl_c=False) as bar:
         for event in events:
             attachment = GCalAttachment(file_url=event.image_url)
-            calendar_event = calendarService.add_event(calendar_id=calendar.id, event=GCalEvent(
+            calendar_event = calendarService.add_event(event=GCalEvent(
                 summary=event.event_name,
                 location=event.venue_info.full_address,
                 start=event.event_datetime,
@@ -49,51 +49,60 @@ def addCalendarEvents(calendarService, calendar, events):
             bar.text = f'-> {calendar_event} Created'
             bar()
 
+
 def get_google_credentials():
-    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    scopes = ['https://www.googleapis.com/auth/calendar']
     secret_file = os.path.join(os.getcwd(), 'service.json')
-    # if os.path.exists('token.json'):
-    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    spreadsheet_id = '1EC_FIA2ZpydWxBsuUaQIUUpcYk3mm6VYWMs4Q4cn26c'
-    range_name = 'Sheet1!A1:D2'
-
-    credentials = Credentials.from_service_account_file(secret_file, scopes=SCOPES)
+    credentials = Credentials.from_service_account_file(secret_file, scopes=scopes)
     return credentials
-    # service = discovery.build('sheets', 'v4', credentials=credentials)
-    # return service
 
 
-def main():
+def clear_calendar(calendar_service):
+    events = calendar_service.get_events()
+
+    with alive_bar(dual_line=True, title='Deleting Old Google Events') as bar:
+        for event in events:
+            bar.text = f'-> Deleting {event.id} {event.summary}'
+            calendar_service.delete_event(event=event)
+            bar()
+
+
+def get_selenium_driver(headless=True):
     options = Options()
     # options.add_experimental_option("detach", True)
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--headless")
-    # options.add_argument("--silent")
+
+    options.add_argument("--headless") if headless else None
     options.add_argument("--disable-infobars")
     options.add_argument("--start-maximized")
-    # options.add_argument("--disable-plugins-discovery")
     driver = webdriver.Chrome(options=options)
-    # creds = get_google_credentials()
-    # print(creds)
+    return driver
 
-    calendar_service = GoogleCalendar('rvamusiccalendar@gmail.com', credentials_path="./credentials.json")
-    calendar = Calendar(
-        'RVA Music Calendar',
-        description='Calendar for all music events in RVA'
-    )
-    calendar = calendar_service.add_calendar(calendar)
-    print(calendar)
+
+def main():
+    driver = get_selenium_driver()
+
+    creds = get_google_credentials()
+    calendar_service = GoogleCalendar(credentials=creds, default_calendar=rva_music_calendar_id)
+
+    clear_calendar(calendar_service)
+
     broad_berry_event_urls = get_broadberry_event_urls(driver)
     the_national_event_urls = get_national_event_urls()
     event_urls = broad_berry_event_urls + the_national_event_urls
 
     events = [*map(lambda event_url: get_event_details(driver, event_url), alive_it(event_urls, title="Scraping events"))]
-    # print([event for event in events])
     print(f"{len(events)} events found")
+
     events = [event for event in events if event is not None]  # If we get errors in scraping we return None, this strips those out
     print(f"{len(events)} events were valid")
-    addCalendarEvents(calendar_service, calendar, events)
+
+    # for event in events:
+    #     print(event.venue_info.city)
+    # events = [event for event in events if event.venue_info.city.strip() != "Richmond"]  # Filter out events that aren't in richmond
+
+    # print(f"{len(events)} events were valid AND in Richmond")
+    addCalendarEvents(calendar_service, events)
     print(f"All of the events have been created")
 
 
