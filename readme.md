e0cd1eae81e341f42d8b1db1a2f570be98f97a30
jzheadley <zephyrinkg@gmail.com>
2023-10-22 00:12:46 -0400

    rewriting the getTightScraper because of bot detection

---
+++ readme.md
@@ -0,0 +1,11 @@
+https://developers.google.com/calendar/api/v3/reference/events/insert
+https://google-calendar-simple-api.readthedocs.io/en/latest/code/event.html
+https://developers.facebook.com/docs/instagram/sharing-to-stories
+https://www.reddit.com/r/androiddev/comments/ehoeo0/any_instagram_api_to_upload_stories/
+https://github.com/dilame/instagram-private-api
+https://github.com/kuzmoyev/google-calendar-simple-api
+
+https://calendar.google.com/calendar/u/0/embed?src=810rm4o2829cdhurresns7a4cc@group.calendar.google.com&ctz=America/New_York
+https://calendar.google.com/calendar/u/3/r/month/2023/7/1
+https://github.com/JZHeadley/RVAMusicCalendar
+https://console.cloud.google.com/iam-admin/serviceaccounts/details/108776304417626015163/permissions?project=rva-music-calendar&supportedpurview=project
--- src/scrapers/getTightScraper.py
+++ src/scrapers/getTightScraper.py
@@ -3,6 +3,7 @@ import time
 from pprint import pprint
 
 import requests
+from alive_progress import alive_bar
 from dateutil.parser import parse
 from selenium.webdriver.common.by import By
 
@@ -54,6 +55,35 @@ def get_event_details(driver, event_url):
         print(f"Problem extracting for {event_url}")
 
 
+def get__getTight_events(driver):
+    try:
+        driver.get(getTightEventsURL)
+        time.sleep(5)
+        handle_popup(driver)
+        driver.find_element(By.CLASS_NAME, "dice_load-more").click()
+        time.sleep(5)
+        event_infos = driver.find_elements(By.XPATH, "//article")
+        events = []
+
+        with alive_bar(len(event_infos), dual_line=True, title='Scraping getTight events', force_tty=True) as bar:
+            for event_info in event_infos:
+                response = json.loads(event_info.find_element(By.TAG_NAME, "script").get_attribute('innerHTML'))[0]
+                event_name = response['name']
+                bar.text = f'-> currently scraping {event_name}'
+                event_date_time = parse(response['startDate'])
+                event_ticket_url = response['url']
+                event_door_open_time = parse(response['doorTime']).time()
+                event_image_url = event_info.find_element(By.TAG_NAME, "img").get_attribute("src")
+                event_description = response['description']
+                event = Event(getTightVenueInfo, event_date_time, event_door_open_time, event_name, event_image_url, event_description, event_ticket_url, color_id="4")
+                events.append(event)
+                bar()
+        return events
+    except:
+        print("Problem occured while scraping The Get Tight Lounge event urls")
+        return []
+
+
 if __name__ == '__main__':
     # selenium_driver = get_selenium_driver(False, True)
     selenium_driver = get_selenium_driver(False)
@@ -61,8 +91,8 @@ if __name__ == '__main__':
     # print(len(urls))
     # print(urls)
 
-    test_event_url = "https://link.dice.fm/i22ef77bf801?pid=8984aee2"
-
+    # test_event_url = "https://link.dice.fm/i22ef77bf801?pid=8984aee2"
+    test_event_url = "https://link.dice.fm/c7d24dc920e0?pid=8984aee2"
     selenium_driver.get(test_event_url)
     test_event = get_event_details(selenium_driver, test_event_url)
     pprint(test_event)
